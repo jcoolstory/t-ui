5f3b49f52752a80b67aa692dd1be8101bb4f65ad
widse <wngud6012@naver.com>
2024-01-04 02:33:39 +0900

    feature(Button): Add Ripple Effect

--- src/component/Button/index.tsx
+++ src/component/Button/index.tsx
@@ -1,25 +1,48 @@
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 import styled from "@emotion/styled";
+import Ripple from './Ripple';
 
-const ButtonControll = styled.button<{ disabled?: boolean; animate?: boolean}>`
-  width: 100%;
-  border-radius: 10px;
-  padding: 10px;
-  border: none;
-  box-shadow: none;
-  &:disabled {
+const ButtonContiner = styled.div`
+    .text {
+        background: transparent;
+        color: #000;
+    };
+    .contained {
+        background: #0064FF;
+        color: #FFF;
+    };
+    .outlined {
+        background: transparent;
+        border: 2px solid #0064FF;
+        color: #000;
+    };
+`
+
+const ButtonControll = styled.button<{disabled?: boolean; animate?: boolean}>`
+    overflow: hidden;
+    outline: none;
+    position: relative;
+    cursor: pointer;
+
+    width: 100%;
+    border-radius: 10px;
+    padding: 10px;
+    border: none;
+    box-shadow: none;
+
+    &:disabled {
     background: #eee !important;
     color: #999 !important;
     border: none !important;
-  }
+    }
 
-  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
-  ${({ animate }) => animate && ButtonAnimation}
+    cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+    ${({ animate }) => animate && ButtonAnimation}
 `;
 
 const ButtonAnimation = `
     transition: all 0.2s ease;
     &:active {
-        background: #f0f0f0 !important;
         transform: scale3d(0.92, 0.95, 1);
     }
 `;
@@ -30,63 +53,26 @@ type ButtonProps = {
   animate?: boolean
   style?: React.CSSProperties
   children?: React.ReactNode
-  onClick?: Function
+  onClick?: () => void 
 }
 
-const setColor = (css:React.CSSProperties, type:any) => {
-    if (type === "text") {
-        css.background = "transparent";
-        css.color = "#000"
-    } else if (type === "contained") {
-        css.background = "#0064ff";
-        css.color = "#fff"
-    } else if (type === "outlined") {
-        css.background = "transparent";
-        css.border = "2px solid #0064ff";
-        css.color = "#000"
+export const Button = ({ type = "contained", style={} , children, onClick, ...props }: ButtonProps) => {
+
+    const handleOnClick = (e:React.MouseEvent) => {
+        e.preventDefault();
+        setTimeout(()=> {
+            if (onClick) onClick();
+        }, 300)
     }
 
-    return css;
-}
-
-export const Button = ({ type = "contained", style={}, children, onClick, ...props }: ButtonProps) => {
-    // const handleClick = () => {
-    //     alert("HI");
-    // }
-
-    const css = setColor(style, type);
-
     return (
-    <div style={{
-        width: "500px",
-        display: "flex",
-        flexDirection: "column",
-    }}>
-        <ButtonControll style={css} {...props}>{children}</ButtonControll>
-        <ButtonControll {...props} style={css} >{children}</ButtonControll>
-    </div>
-    );
-};
-
-export const ButtonSample = ({ type = "contained", disabled=false, animate=false, style={}, children, onClick }: ButtonProps) => {
-    const handleClick = () => {
-        alert("HI");
-    }
-
-    const css = setColor(style, type);
-
-    return (
-    <div style={{
-        width: "500px",
-        display: "flex",
-        flexDirection: "column",
-    }}>
-        <ButtonControll style={css} onClick={handleClick}>{children}</ButtonControll>
-        <ButtonControll style={css} disabled onClick={handleClick}>{children}</ButtonControll>
-        <ButtonControll style={css} animate onClick={handleClick}>{children}</ButtonControll>
-    </div>
+    <ButtonContiner>
+        <ButtonControll className={type} onClick={handleOnClick} {...props}>
+            <Ripple color={"#666"} duration={600} />
+            {children}
+        </ButtonControll>
+    </ButtonContiner>
     );
 };
 
 export default Button
-
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { isArgumentsObject } from 'util/types';
 import Button from '../component/Button';
 
 const meta: Meta<typeof Button> = {
@@ -12,21 +13,36 @@ const meta: Meta<typeof Button> = {
   },
 };
 
-
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+  render: ({...args}) => {
+    return (
+      <div style={{
+        width: 150,
+        height: 50
+      }}>
+        <Button onClick={()=> {alert("hi")}} {...args}>Button</Button>
+      </div>
+    )
+  }
+};
 
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
-export const Primary: Story = {
+export const Sample1: Story = {
   render: () => {
     return (
-        <Button type="text" animate>
+      <div style={{
+        width: "500px",
+        display: "flex",
+        flexDirection: "column",
+       }}>
+        <Button type="text" animate onClick={()=> {alert("hi");}}>
           <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", gap: 10}}>
             <div style={{width: 30, height: 30, borderRadius:"50%", backgroundColor:"#168ed9"}}></div>
             <div style={{display:"flex", flexDirection:"column", textAlign:"left", flex: 1}}>
@@ -36,7 +52,8 @@ export const Primary: Story = {
             <div style={{width: 40, height:30, borderRadius:5, backgroundColor:"#EEE", display: "flex", alignItems:"center", justifyContent: "center"}}>송금</div>
           </div>
         </Button>
-      )
+      </div>
+  )
   }
 };
 
