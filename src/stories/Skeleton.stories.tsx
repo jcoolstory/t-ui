40dc58032e0f6bb78b5e6f17a362bcbd71be8f0f
정시원 <sw.jeong@fosskorea.com>
2024-01-11 19:26:10 +0900

    skeleton added

---
+++ src/stories/Skeleton.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Skeleton from "../component/Skeleton";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Skeleton",
+  component: Skeleton,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+
+export const Primary: Story = {
+  args: {
+    variant: "round",
+    width: "100px",
+    height: "100px"
+  },
+  render: (arg) => {
+    return <Skeleton {...arg}/>;
+  },
+};
+
+
+export const Rect: Story = {
+  args: {
+    variant: "react",
+    width: "200px",
+    height: "20px"
+  },
+  render: (arg) => {
+    return <Skeleton {...arg}/>;
+  },
+};
+
+export const Circle: Story = {
+  args: {
+    variant: "circular",
+    width: "200px",
+    height: "200px"
+  },
+  render: (arg) => {
+    return <Skeleton {...arg}/>;
+  },
+};
+
+
+export const Round: Story = {
+  args: {
+    variant: "round",
+    width: "200px",
+    height: "200px",
+    radius: 20
+  },
+  render: (arg) => {
+    return <Skeleton {...arg}/>;
+  },
+};
